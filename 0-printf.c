f73cad55d6549202243df12a915c9a629ccef710
Francis Chege <francischege602@gmail.com>
2023-07-24 09:33:14 -0700

    _printf

---
+++ 0-printf.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _printf - prints to the std output depending on the specifier fiven as argument
+ * @format : string containing the format specifiers
+ * Return: strlen of whatever is printed
+ */
+int _printf(const char *format, ...)
+{
+	char ch;
+	char *str;
+	size_t len;
+	int ret = 0;
+	va_list list;
+
+	va_start(list, format);
+	for (len = 0; len < strlen(format); len += 2)
+	{
+		if (format[len] == '%' && format[len + 1] == 'c')
+		{
+			ch = va_arg(list, int);
+			printf("%c", ch);
+			ret += 1;
+		}
+		else if (format[len] == '%' && format[len + 1] == 's')
+		{
+			str = va_arg(list, char*);
+			printf("%s", str);
+			ret += strlen(str);
+		}
+	}
+	return (ret);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+This is our own version of the printf function fiund in the c standard library.
+It accepts different format specifiers similar to those of the standard printf function.
